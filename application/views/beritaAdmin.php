50758ec102dbe4505cd0513f2391dc9787b61208
fadri <fadrimerdianto@gmail.com>
2015-05-22 22:40:02 +0700

    bikin tabel berita admin eheheh

--- application/views/beritaAdmin.php
+++ application/views/beritaAdmin.php
@@ -18,25 +18,43 @@
                 <h2><i class="glyphicon glyphicon-info-sign"></i> Berita</h2>
 
                 <div class="box-icon">
-                    <a href="#" class="btn btn-setting btn-round btn-default"><i
-                            class="glyphicon glyphicon-cog"></i></a>
-                    <a href="#" class="btn btn-minimize btn-round btn-default"><i
-                            class="glyphicon glyphicon-chevron-up"></i></a>
-                    <a href="#" class="btn btn-close btn-round btn-default"><i
-                            class="glyphicon glyphicon-remove"></i></a>
+                    <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                    <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
                 </div>
             </div>
             <div class="box-content row">
-                <div class="col-lg-7 col-md-12">
+                <div class="col-md-12">
+                <table class='table table-striped table-bordered bootstrap-datatable datatable responsive'>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Berita</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
                 <?php foreach ($berita->result() as $row) {
-                    echo "<table> 
+                    echo " 
                         <tr>
                         <td> <a href='#'>". $row->id_berita." </td> <td> <a href='#'> " .$row->judul_berita.  
-                        "</td></a><br>".
-                        "</tr>
-                        <tr><td></td><td>aaaaaaaaaaaa</td>".
-                        "</table>";
+                        "</td></a><br><td class='center'>
+                            <a class='btn btn-success' href='#'>
+                                <i class='glyphicon glyphicon-zoom-in icon-white'></i>
+                                View
+                            </a>
+                            <a class='btn btn-info' href='#'>
+                                <i class='glyphicon glyphicon-edit icon-white'></i>
+                                Edit
+                            </a>
+                            <a class='btn btn-danger' href='#'>
+                                <i class='glyphicon glyphicon-trash icon-white'></i>
+                                Delete
+                            </a>
+                        </td>
+                        </tr>
+                        ";
                 }?>
+                </table>
                 </div>
 
                 <div class="col-lg-5 col-md-12 visible-xs center-text">
