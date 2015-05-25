f4a5e0d4ae34c68c59b28fdc5d43f3cb63bb4fad
Dinar Winia Mahandhira <dinar.mahandhira@gmail.com>
2015-05-25 14:43:55 +0700

    update registrasi

--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -28,11 +28,21 @@
 		</tr>
 		<tr> 
 			<td><label> Umur Kehamilan </label></td>
-			<td><input type="text" name="umur" id="umur" required placeholder="Bulan"/> </td>
+			<td><select>
+			  <option value="1">1 bulan</option>
+			  <option value="2">2 bulan</option>
+			  <option value="3">3 bulan</option>
+			  <option value="4">4 bulan</option>
+			  <option value="5">5 bulan</option>
+			  <option value="6">6 bulan</option>
+			  <option value="7">7 bulan</option>
+			  <option value="8">8 bulan</option>
+			  <option value="9">9 bulan</option>
+			</select> </td>
 		</tr>
 		<tr> 
 			<td></td>
-			<td><input id="btn" type="submit" class="large blue tt-button" value="Kirim"></td>
+			<td><input id="btn" type="submit" class="large blue tt-button" value="Daftar" style="margin-left:10px;"></td>
 		</tr>
 
 		</table>
